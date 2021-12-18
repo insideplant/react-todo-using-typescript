2bdf59fc19ed66fc74cb2f076086774f5a4ae2c9
insideplant <kami.10pearl@gmail.com>
2021-12-19 01:33:44 +0900

    delete not needed import

--- src/components/organisms/todo/TodoDetailModal.tsx
+++ src/components/organisms/todo/TodoDetailModal.tsx
@@ -1,17 +1,12 @@
 import React, { useContext } from "react";
 import { VFC } from "react";
-import { 
-  Modal,
-  ModalOverlay,
-  ModalContent,
-} from '@chakra-ui/react'
 
 import { ModalContext } from "../../../providers/ModalProvider";
 import { CreateModal } from "../../molecules/todo/createModal";
 import { EditModal } from "../../molecules/todo/editModal";
 
 export const TodoDetailModal: VFC = () => {
-  const { isOpen, onClose, isEditing } = useContext<any>(ModalContext);
+  const { isEditing } = useContext<any>(ModalContext);
 
 
   return (
